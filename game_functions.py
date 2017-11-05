f1bba6f122751f830a3839c4cc2c3fb2202c71d2
Vincent Jin <vjin@cisco.com>
2017-11-04 17:03:46 -0700

    Make the ship able to fire bullets

--- game_functions.py
+++ game_functions.py
@@ -1,23 +1,20 @@
 import sys
 
 import pygame
+from bullet import Bullet
 
-def check_events(ship):
-    """Responde to keypress and mouse events"""
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
-        elif event.type == pygame.KEYUP:
-            check_keyup_events(event, ship)
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Responde to key presses"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # Create a new bullet and add it to the bullets group
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
 
 def check_keyup_events(event, ship):
     """Responde to key releases"""
@@ -26,9 +23,26 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def update_screen(ai_settings, screen, ship):
+
+def check_events(ai_settings, screen, ship, bullets):
+    """Responde to keypress and mouse events"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+
+def update_screen(ai_settings, screen, ship, bullets):
     # Redraw the screen during each pass through the loop
     screen.fill(ai_settings.bg_color)
+
+    # Redraw all bullets behind ship and aliens
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
     ship.blitme()
 
     # Make the most recent drawn screen visible
